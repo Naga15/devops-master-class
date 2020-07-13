ed947b330ca385548a131656aa2db9f1e9ec5d07
NagaChaitanya <Nagac@Naga156>
2020-07-12 21:54:40 -0700

    naga-001

--- terraform/backup/01-terraform-basics/main.tf
+++ terraform/backup/01-terraform-basics/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
     region = "us-east-1"
-    version = "~> 2.70"
+    version = "~> 2.46"
 }
 
 # plan - execute 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-in28minutes-rangak-002"
+    bucket = "my-s3-bucket-in28minutes-naga-001"
     versioning {
         enabled = true
     }
